c95baecfa3c1b26a8659f579b6ee6ffc06baf6a5
RJay Sharma <rsharma@beesolvertechnology.com>
2022-12-12 11:46:33 +0530

    implemented image generator using openai

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.port || 5000;
+
+const app = express();
+
+// enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on ${port}`));
